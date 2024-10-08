8460dff9cc67e0cff0eb28a9c9bbedc78496b6d0
Hyurim <kyo2301@naver.com>
2024-10-08 14:39:16 +0900

    fix: update DTO contents

--- backend/sulomon/src/main/java/com/sulomon/web/dto/PostDTO.java
+++ backend/sulomon/src/main/java/com/sulomon/web/dto/PostDTO.java
@@ -15,7 +15,6 @@ import java.util.Set;
 public class PostDTO {
 
     private int postId;                // 게시물 ID
-    private String privatePassword;
     private int userNum;               // 작성자 ID
     private String title;              // 게시물 제목
     private String content;            // 게시물 내용
@@ -23,6 +22,7 @@ public class PostDTO {
     private int views;                 // 조회수
     private String imagePath;          // 이미지 파일 경로
     private String visibility;         // 공개/비공개 상태 (VISIBLE, HIDDEN)
+    private String privatePassword;    // 게시글 비밀번호
     private LocalDateTime createdAt;   // 생성 시간
     private LocalDateTime updatedAt;   // 수정 시간
     private String status;             // 게시물 상태 (ACTIVE, DELETED)
