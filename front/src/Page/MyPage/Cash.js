399ed02b2c6d92632142d9de1d5392fe1b42a7f1
Hyurim <kyo2301@naver.com>
2024-10-21 20:34:59 +0900

    feat : cash.js add

---
+++ front/src/Page/MyPage/Cash.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MyPageContainer from "./Components/MyPageContainer";
+import styled from "styled-components";
+
+const DashBoard = () => {
+  return <div></div>;
+};
+const Cash = () => {
+  return (
+    <MyPageContainer>
+      <DashBoard />
+    </MyPageContainer>
+  );
+};
+
+export default Cash;
