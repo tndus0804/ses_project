8f6f4a9f4da59262398c27b45b355ab1a03eb889
김수연 <57806822+tndus0804@users.noreply.github.com>
2024-10-28 18:54:30 +0900

    feat: Define post service interface for post operations

--- backend/sulomon/src/main/java/com/sulomon/web/service/PostService.java
+++ backend/sulomon/src/main/java/com/sulomon/web/service/PostService.java
@@ -1,9 +1,20 @@
 package com.sulomon.web.service;
 
+import java.util.List;
+
 import com.sulomon.web.dto.PostDTO;
 
 public interface PostService {
-    void writePost(PostDTO postDTO);
-    void updatePost();
-    void deletePost();
+	// 게시글 작성, 수정, 삭제
+		void writePost(PostDTO postDTO, String username);
+		boolean updatePost(PostDTO postDTO, String username);
+		void deletePost(PostDTO postDTO, String username);
+
+		// 게시글 조회
+		PostDTO getPost(int postNum);
+		// 게시글 전체 조회
+		List<PostDTO> selectAll();
+
+		// 게시글 검색
+		List<PostDTO> searchPost(String keyword);
 }
