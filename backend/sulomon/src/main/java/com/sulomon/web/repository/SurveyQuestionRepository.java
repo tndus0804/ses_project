e2084240e66575112e7782bb6975c0548e47c2bd
김수연 <57806822+tndus0804@users.noreply.github.com>
2024-10-28 19:52:31 +0900

    feat: Add method to find survey questions by survey in SurveyQuestionRepository

--- backend/sulomon/src/main/java/com/sulomon/web/repository/SurveyQuestionRepository.java
+++ backend/sulomon/src/main/java/com/sulomon/web/repository/SurveyQuestionRepository.java
@@ -1,10 +1,15 @@
 package com.sulomon.web.repository;
 
+import com.sulomon.web.entity.SurveyEntity;
 import com.sulomon.web.entity.SurveyQuestionEntity;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SurveyQuestionRepository extends JpaRepository<SurveyQuestionEntity, Integer> {
     // JpaRepository<Entity 클래스, ID 타입>을 상속받아 기본적인 CRUD 메서드를 제공
+	List<SurveyQuestionEntity> findBySurvey(SurveyEntity surveyEntity);
 }
